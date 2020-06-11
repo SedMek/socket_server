4928363ece6edcd50c8eb8ce544ef3802e65d0e0
Seddik <s.mekki@kayrros.com>
2020-06-11 22:01:55 +0200

    added calls

--- app.js
+++ app.js
@@ -12,32 +12,40 @@ const server = http.createServer(app);
 
 const io = socketIo(server);
 
-let interval;
+const users = {};
 
-io.on('connection', (socket) => {
-  console.log("New client connected");
-  socket.on("disconnect", () => {
-    console.log("Client disconnected");
-  });
-  socket.on('play/pause', (msg) => {
-    console.log("play/pause: ", msg);
-    io.emit('play/pause', msg);
-  });
-  socket.on('sync', (time) => {
-    console.log("sync: ", time);
-    io.emit('sync', time);
-  });
-  socket.on('seekTo', (time) => {
-    console.log("seekTo: ", time);
-    io.emit('seekTo', time);
-  });
-  socket.on('shareUrl', (url) => {
-    console.log("shareUrl: ", url);
-    io.emit('shareUrl', url);
-  });
+io.on("connection", (socket) => {
+	if (!users[socket.id]) {
+		users[socket.id] = socket.id;
+	}
+	socket.emit("yourID", socket.id);
+	console.log("New client connected:", socket.id);
+	socket.on("disconnect", () => {
+		console.log("Client disconnected:", socket.id);
+	});
+	socket.on("play/pause", (msg) => {
+		console.log("play/pause: ", msg);
+		io.emit("play/pause", msg);
+	});
+	socket.on("sync", (time) => {
+		console.log("sync: ", time);
+		io.emit("sync", time);
+	});
+	socket.on("seekTo", (time) => {
+		console.log("seekTo: ", time);
+		io.emit("seekTo", time);
+	});
+	socket.on("shareUrl", (url) => {
+		console.log("shareUrl: ", url);
+		io.emit("shareUrl", url);
+	});
+	socket.on("callUser", (data) => {
+		console.log("Incoming call from: ", socket.id);
+		io.emit("hey", data);
+	});
+	socket.on("acceptCall", (data) => {
+		io.emit("callAccepted", data);
+	});
 });
-
-
-
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
